addc95cebb44b5ecc4c459e58b9035af1c7cee82
Alexis Bouvry <bouvryalexis@gmail.com>
2025-07-29 10:36:41 +0200

    create employeelist page

--- src/pages/EmployeeList/index.jsx
+++ src/pages/EmployeeList/index.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function EmployeeList() {
   return (
-    <div>Employee</div>
-  )
+    <main>
+      <h1>Employee List</h1>
+      <input type="search" placeholder="Search..." />
+      <Link className="redirect" to="/">
+        <p>Home</p>
+      </Link>
+    </main>
+  );
 }
 
-export default EmployeeList
+export default EmployeeList;
