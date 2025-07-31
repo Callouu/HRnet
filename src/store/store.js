ed9a3a57a8ed7ab7ccfa8a52064ed973f85df7fd
Alexis Bouvry <bouvryalexis@gmail.com>
2025-07-31 19:05:16 +0200

    create redux store for employee list

---
+++ src/store/store.js
@@ -0,0 +1,27 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import employeeReducer from './employeeSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  // Ignore non-serializable values from redux-persist
+  blacklist: ['register']
+};
+
+const persistedReducer = persistReducer(persistConfig, employeeReducer);
+
+export const store = configureStore({
+  reducer: {
+    employees: persistedReducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST'],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
