5aa68ba8d76b40c0c61428472b1184d5638448de
Alexis Bouvry <bouvryalexis@gmail.com>
2025-08-01 23:57:53 +0200

    add dropdown component

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 
-function Dropdown() {
+function Dropdown({ name, options, value, onChange }) {
   return (
-    <div>Dropdown</div>
+    <select 
+      name={name}
+      value={value}
+      onChange={onChange}
+      required
+    >
+      <option value="">Select...</option>
+      {options.map((option, index) => {
+        // Supporte { label, value } ou string
+        const optionValue = option.value !== undefined ? option.value : option;
+        const displayText = option.label !== undefined ? option.label : option;
+        return (
+          <option key={index} value={optionValue}>
+            {displayText}
+          </option>
+        );
+      })}
+    </select>
   )
 }
 
