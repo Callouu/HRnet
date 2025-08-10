5fea44f879ed8dba14e7630494afc1d2d781bef0
Alexis Bouvry <bouvryalexis@gmail.com>
2025-08-11 01:01:54 +0200

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/HRnet/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        404: resolve(__dirname, 'index.html') // copie pour GH Pages
-      }
-    }
-  }
-});
+})
