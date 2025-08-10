076b06a351a9bcff69bd04f351c0d0853f60181f
Alexis Bouvry <bouvryalexis@gmail.com>
2025-08-10 20:43:42 +0200

    jest setup

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,15 @@
 export default {
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  setupFilesAfterEnv: ['@testing-library/jest-dom', './jest.setup.js'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '\\.(css|scss)$': 'identity-obj-proxy'
   },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['@babel/preset-env', '@babel/preset-react'] }]
-  }
+  },
+  // Allow ES modules in node_modules/react-modal-ab to be transformed by Babel
+  transformIgnorePatterns: [
+    '/node_modules/(?!react-modal-ab)'
+  ]
 };
